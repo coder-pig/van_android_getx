22516c0fa8b97ad9d945018470f8030d7ce29aa7
CoderPig <779878443@qq.com>
2024-07-16 17:52:10 +0800

    feat: 使用KeepAliveWrapper包裹PageView的子页面避免切页面重建，首页添加悬浮按钮相关逻辑

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:van_android_getx/core/utils/logger_utils.dart';
 import 'package:van_android_getx/features/browser/browser_page.dart';
 import 'package:van_android_getx/features/home/vm/home_vm.dart';
 import 'package:van_android_getx/features/home/widgets/article_item_widget.dart';
@@ -11,35 +10,52 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    LogUtil.d("首页重建了");
+    var scrollController = ScrollController();
+    var showFab = false.obs; // 悬浮按钮是否显示的可观察变量
     final vm = Get.put(HomeVM());
-    return RefreshIndicator(
-        onRefresh: () => vm.fetchArticleList(isRefresh: true),
-        // 包裹一个可观察组件
-        child: Obx(() {
-          // ScrollController 实例必须写在这里，写在VM里，会导致无法下拉刷新
-          var scrollController = ScrollController();
-          scrollController.addListener(() {
-            // 列表滑动到底部加载更多
-            if (scrollController.position.pixels == scrollController.position.maxScrollExtent) {
-              vm.fetchArticleList();
-            }
-          });
-          return ListView.builder(
-              controller: scrollController,
-              // 列表长度为数据长度+1,0用来显示Banner
-              itemCount: vm.homeArticleInfoItems.length + 1,
-              itemBuilder: (context, index) {
-                if (index == 0) {
-                  return AutoScrollBannerWidget(
-                      imageUrls: vm.bannerItems.map((element) => element.imagePath).toList(),
-                      onTap: (pos) {
-                        Get.to(BrowserPage(url: vm.bannerItems[pos].url));
-                      });
-                } else {
-                  return ArticleItemWidget(articleInfo: vm.homeArticleInfoItems[index - 1]);
+    return Scaffold(
+        floatingActionButton: Obx(() => showFab.value
+            ? FloatingActionButton(
+                onPressed: () {
+                  scrollController.animateTo(
+                    0,
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeOut,
+                  );
+                },
+                child: const Icon(Icons.arrow_upward),
+              )
+            : Container()),
+        body: RefreshIndicator(
+            onRefresh: () => vm.fetchArticleList(isRefresh: true),
+            child: Obx(() {
+              scrollController.addListener(() {
+                // 根据滑动距离控制悬浮按钮的显示隐藏
+                if (scrollController.position.pixels > 200) {
+                  showFab.value = true;
+                } else if (scrollController.position.pixels <= 200) {
+                  showFab.value = false;
+                }
+                // 列表滑动到底部加载更多
+                if (scrollController.position.pixels == scrollController.position.maxScrollExtent) {
+                  vm.fetchArticleList();
                 }
               });
-        }));
+              return ListView.builder(
+                  controller: scrollController,
+                  // 列表长度为数据长度+1,0用来显示Banner
+                  itemCount: vm.homeArticleInfoItems.length + 1,
+                  itemBuilder: (context, index) {
+                    if (index == 0) {
+                      return AutoScrollBannerWidget(
+                          imageUrls: vm.bannerItems.map((element) => element.imagePath).toList(),
+                          onTap: (pos) {
+                            Get.to(BrowserPage(url: vm.bannerItems[pos].url));
+                          });
+                    } else {
+                      return ArticleItemWidget(articleInfo: vm.homeArticleInfoItems[index - 1]);
+                    }
+                  });
+            })));
   }
 }
--- lib/features/main/widget/index_container_widget.dart
+++ lib/features/main/widget/index_container_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:van_android_getx/features/home/pages/home_page.dart';
+import 'package:van_android_getx/widgets/keep_alive_wrapper.dart';
 import 'package:van_android_getx/wx_article_page.dart';
 
 import '../../../other_page.dart';
@@ -13,34 +14,15 @@ class IndexContainerWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final MainVM vm = Get.find<MainVM>();
     return Expanded(
-        child: PageView.builder(
-          controller: vm.mainPageController,
-          onPageChanged: (index) {
-            vm.tabIndex.value = index;
-          },
-          itemCount: 3,
-          itemBuilder: (context, index) {
-            switch(index) {
-              case 0:
-                return const KeyedSubtree(
-                  key: PageStorageKey<String>('HomePage'),
-                  child: HomePage(),
-                );
-              case 1:
-                return const KeyedSubtree(
-                  key: PageStorageKey<String>('WxArticlePage'),
-                  child: WxArticlePage(),
-                );
-              case 2:
-                return const KeyedSubtree(
-                  key: PageStorageKey<String>('OtherPage'),
-                  child: OtherPage(),
-                );
-              default:
-                return const SizedBox.shrink(); // 如果索引不匹配，返回一个空的占位Widget
-            }
-          }
-        )
-    );
+        child: PageView(
+            controller: vm.mainPageController,
+            onPageChanged: (index) {
+              vm.tabIndex.value = index;
+            },
+            children: const [
+              KeepAliveWrapper(child: HomePage()),
+              KeepAliveWrapper(child: WxArticlePage()),
+              KeepAliveWrapper(child: OtherPage()),
+        ]));
   }
 }
